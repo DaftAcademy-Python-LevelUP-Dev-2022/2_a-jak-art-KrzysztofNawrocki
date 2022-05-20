d5305c3ee8a3336b252092d009b3ab6160432561
Krzysztof Nawrocki <k.nawrocki@hotmail.com>
2022-05-21 00:25:12 +0200

    zadanie 2.2

--- file.py
+++ file.py
@@ -5,8 +5,25 @@ def greeter(greet_person):
     return wrapper
 
 
-def sums_of_str_elements_are_equal(func):
-    pass
+def sums_of_str_elements_are_equal(condition):
+    def wrapper(*args):
+        numbers_list = condition(*args).split(" ")
+        sum_list = [0 for i in range(len(numbers_list))]
+        for index, number in enumerate(numbers_list):
+            for digit in number:
+                if digit == "-":
+                    continue
+                sum_list[index] += int(digit)
+        
+        for index, num in enumerate(numbers_list):
+            if num[0] == "-":
+                sum_list[index] *= -1
+
+        if sum_list[0] == sum_list[1]:
+            return f"{sum_list[0]} == {sum_list[1]}"
+        else:
+            return f"{sum_list[0]} != {sum_list[1]}"
+    return wrapper
 
 
 def format_output(*required_keys):
